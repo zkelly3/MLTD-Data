881c7551618dd66335340ff811ff509b5c55be0b
white <zkelly3@yahoo.com.tw>
2021-04-25 15:53:08 +0800

    watch frontend change.

--- web/file_watcher.sh
+++ web/file_watcher.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 trap "pkill -TERM -P $$" EXIT
-while inotifywait -e close_write -r static templates; do
+while inotifywait -e close_write -r static templates frontend/src; do
+    npm run build --prefix frontend
     uwsgi --reload uwsgi.pid
 done
