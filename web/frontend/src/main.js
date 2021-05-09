0842e57f4b77a5b953aa1c8f95dcd910982c3b49
Tian-Yi Lin <sh961014@gmail.com>
2021-05-09 13:15:04 +0800

    Use sass and override link color of bootstrap.

--- web/frontend/src/main.js
+++ web/frontend/src/main.js
@@ -1,6 +1,6 @@
 import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
+import './style.scss'
 
 import { createApp } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
